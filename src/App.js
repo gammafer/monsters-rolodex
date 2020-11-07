dd2ba1cb5ff9478768ae070e86e44f8b2a2b0d92
gammafer <60357762+gammafer@users.noreply.github.com>
2020-11-07 16:17:07 +0100

    adding React files

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {CardList} from './component/Card/card-list.component';
+import { Component } from 'react';
+import { SearchBox } from './component/search-box/search-box';
+
+ 
+class App extends Component {
+ constructor(){
+  super();
+  this.state={
+    monster:[],
+    searchField:''
+  };}
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(resp=>resp.json()).
+    then(users=>this.setState({monster:users}));
+  }
+  render(){
+    const {monster, searchField} = this.state;
+    const filteredMonsters = monster.filter((monst)=>
+      monst.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
+  return (
+    <div className="App">
+       <SearchBox
+       placeholder='search monsters'
+       handleChange={
+        e=>this.setState({searchField:e.target.value},()=>console.log(this.state)
+        )}
+       ></SearchBox>
+      <CardList monster={filteredMonsters}>{
+       
+      }</CardList>
+      
+    </div>
+  );}
+}
+
+export default App;
